c425b876a1b61b73755ef306e70a31a3897a9acf
Jeff Blaine <jblaine@kickflop.net>
2013-09-18 09:10:53 -0400

    Update stable version of PuTTY
    Per http://lists.tartarus.org/pipermail/putty-announce/2013/000018.html
    
    Security release. Recommended upgrade for "everyone"

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['putty']['version']      = "0.62"
+default['putty']['version']      = "0.63"
 default['putty']['url']          = "http://the.earth.li/~sgtatham/putty/#{default['putty']['version']}/x86/putty-#{default['putty']['version']}-installer.exe"
 default['putty']['package_name'] = "PuTTY version #{default['putty']['version']}"
 
